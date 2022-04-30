bdc1a7e5fa66ef58b102d32717ac9e7da72e5509
Denis <durnker@gmail.com>
2022-04-30 16:33:48 +0300

    added color picker

--- javascript.js
+++ javascript.js
@@ -11,9 +11,10 @@
 //grid
 //slider (slider sets the value only for one of the variables. value is copied to the other one )
 //buttons
-//reset
+//reset (RESET BACKGROUND COLOR FOR ALL DIVS TO WHITE )
 
-
+//GLOBAL VARIABLES
+let color;
 //SLIDER LOGIC
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
@@ -24,20 +25,32 @@ slider.oninput = function() {
   return (slider.value);
 }
 //SLiDER LOGIC
+//ColorPcker
+let colorInput = document.querySelector("#color")
+let hexInput = document.querySelector("#hex")
+hexInput.innerHTML = colorInput.value;
+
+colorInput.oninput = function() {
+    color = colorInput.value;
+    console.log(colorInput.value)
+    hexInput.innerHTML = this.value;
+    return(color);
+}
+
 
 //GRID
 function makeGrid(size) {
 let grid = document.querySelector(".grid");
-let square = grid.querySelectorAll("div");
-square.forEach((div) => div.remove());
-grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+let square = grid.querySelectorAll("div"); // removes all divs before the (1) create div is run
+square.forEach((div) => div.remove());     // helps the grid to remain the correct size by resetting squares
+grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;  //creates the grid based on size variable
 grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-let amount = size * size;
+let amount = size * size;                   
 for(let i=0; i<amount; i++) {
-    let square = document.createElement("div");
-    square.addEventListener("mouseover", colorSquare());
-    square.style.backgroundColor = "turquoise";
-    grid.insertAdjacentElement("beforeend", square);
+    let square = document.createElement("div"); // (1)
+    square.addEventListener("mouseover", colorSquare); 
+    square.style.backgroundColor = "white"; // background color for default squares
+    grid.insertAdjacentElement("beforeend", square);  //injects squares before the end 
     }
 }
 makeGrid(16);
@@ -45,8 +58,9 @@ makeGrid(16);
 function changeSize(input) {
     makeGrid(input);
 }
-
-function colorSquare(color) {
-    square.style.backgroundColor="color"
+//GRID
+function colorSquare() {
+    this.style.backgroundColor=colorInput.value;
+    console.log(colorInput.value);
 
 }
